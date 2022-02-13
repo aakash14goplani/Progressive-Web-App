995f49dc10c8858b730f21b42934a695a911840a
Aakash <goplaniaakash14@gmail.com>
2022-02-13 21:34:46 +0530

    added dummy data showcasing promises and fetch

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,7 +1,11 @@
 this.addEventListener('install', (e) => {
-    console.log('Service Worker: Installed', e);
+  console.log('Service Worker: Installed', e);
 });
 
 this.addEventListener('activate', (e) => {
-    console.log('Service Worker: Activated', e);
+  console.log('Service Worker: Activated', e);
+});
+
+this.addEventListener('fetch', (e) => {
+  console.log('Service Worker: Fetch', e);
 });
--- public/src/js/app.js
+++ public/src/js/app.js
@@ -1,5 +1,43 @@
 if ('serviceWorker' in navigator) {
-	navigator.serviceWorker.register('/service-worker.js').then(() => {
-		console.log('service worker registered');
-	});
+  navigator.serviceWorker.register('/service-worker.js').then(() => {
+    console.log('service worker registered');
+  });
 }
+
+var deferredPrompt;
+
+this.addEventListener('beforeinstallprompt', (e) => {
+  console.log('beforeinstallprompt', e);
+  e.preventDefault();
+  deferredPrompt = e;
+  return false;
+});
+
+/* setTimeout(() => {
+  console.log('within set timeout');
+});
+console.log('outside set timeout');
+
+const promise = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    // resolve('within promise');
+    reject('promise rejected');
+  });
+});
+
+promise.then((value) => {
+  console.log('promise value', value);
+}).catch((error) => {
+  console.log('promise error', error);
+}).finally(() => { }).then(() => {
+  console.log('promise finally');
+}); */
+
+fetch('http://httpbin.org/bytes/5').then((response) => {
+  console.log('API RESPONSE: ', response);
+  return response.text();
+}).then((data) => {
+  console.log('API TEXT DATA: ', data);
+}).catch((error) => {
+  console.log('API ERROR: ', error);
+});
