4a0357e7e3a37e566a3cca9b7faae94cdcfcd9a8
Aakash <goplaniaakash14@gmail.com>
2022-02-22 01:34:45 +0530

    Working with caching - static and dynamic cachimg

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,11 +1,64 @@
+let STATIC_CACHE = 'static-v6';
+let DYNAMIC_CACHE = 'dynamic-v4';
+
 this.addEventListener('install', (e) => {
   console.log('Service Worker: Installed', e);
+  e.waitUntil(
+    caches.open(STATIC_CACHE).then((cache) => {
+      console.log('Service Worker: Caching Files');
+      /* cache.add('/');
+      cache.add('/index.html');
+      cache.add('/src/js/app.js'); */
+      cache.addAll([
+        '/',
+        '/index.html',
+        'offline.html',
+        '/favicon.ico',
+        '/manifest.json',
+        '/src/js/app.js',
+        '/src/js/feed.js',
+        '/src/js/material.min.js',
+        '/src/css/app.css',
+        '/src/css/feed.css',
+        '/src/images/main-image.jpg',
+        'https://fonts.googleapis.com/css?family=Roboto:400,700',
+        'https://fonts.googleapis.com/icon?family=Material+Icons',
+        'https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css'
+      ]);
+    })
+  );
 });
 
 this.addEventListener('activate', (e) => {
   console.log('Service Worker: Activated', e);
+  e.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(cacheNames.map((cacheName) => {
+        if (cacheName !== STATIC_CACHE && cacheName !== DYNAMIC_CACHE) {
+          console.log('Service Worker: Clearing Old Cache: ', cacheName);
+          return caches.delete(cacheName);
+        }
+      }));
+    })
+  );
 });
 
 this.addEventListener('fetch', (e) => {
-  console.log('Service Worker: Fetch', e);
+  e.respondWith(
+    caches.match(e.request).then((response) => {
+      if (response) {
+        console.log('returning value from cache: ', response, ', request: ', e.request);
+      }
+      return response || fetch(e.request).then((response) => {
+        return caches.open(DYNAMIC_CACHE)
+          .then((cache) => {
+            cache.put(e.request.url, response.clone());
+            return response;
+          })
+      }).catch((error) => {
+        console.log('returning fallback offline page: ', error);
+        return caches.match('/offline.html');
+      });
+    })
+  );
 });
--- public/src/js/feed.js
+++ public/src/js/feed.js
@@ -1,12 +1,14 @@
 var shareImageButton = document.querySelector('#share-image-button');
 var createPostArea = document.querySelector('#create-post');
 var closeCreatePostModalButton = document.querySelector('#close-create-post-modal-btn');
+var sharedMomentsArea = document.querySelector('#shared-moments');
 
 function openCreatePostModal() {
   createPostArea.style.display = 'block';
   if (deferredPrompt) {
     deferredPrompt.prompt();
-    deferredPrompt.userChoice.then((choiceResult) => {
+
+    deferredPrompt.userChoice.then(function (choiceResult) {
       console.log(choiceResult.outcome);
 
       if (choiceResult.outcome === 'dismissed') {
@@ -24,6 +26,58 @@ function closeCreatePostModal() {
   createPostArea.style.display = 'none';
 }
 
+// user initiated cache storage
+function onSaveButtonClicked(event) {
+  if ('caches' in window) {
+    caches.open('user-requested')
+      .then((cache) => {
+        cache.add('https://httpbin.org/get');
+        cache.add('/src/images/sf-boat.jpg');
+      });
+  }
+}
+
 shareImageButton.addEventListener('click', openCreatePostModal);
 
 closeCreatePostModalButton.addEventListener('click', closeCreatePostModal);
+
+function createCard() {
+  var cardWrapper = document.createElement('div');
+  cardWrapper.className = 'shared-moment-card mdl-card mdl-shadow--2dp';
+
+  var cardTitle = document.createElement('div');
+  cardTitle.className = 'mdl-card__title';
+  cardTitle.style.backgroundImage = 'url("/src/images/sf-boat.jpg")';
+  cardTitle.style.backgroundSize = 'cover';
+  cardTitle.style.height = '180px';
+  cardWrapper.appendChild(cardTitle);
+
+  var cardTitleTextElement = document.createElement('h2');
+  cardTitleTextElement.style.color = 'white';
+  cardTitleTextElement.className = 'mdl-card__title-text';
+  cardTitleTextElement.textContent = 'San Francisco Trip';
+  cardTitle.appendChild(cardTitleTextElement);
+
+  var cardSupportingText = document.createElement('div');
+  cardSupportingText.className = 'mdl-card__supporting-text';
+  cardSupportingText.textContent = 'In San Francisco';
+  cardSupportingText.style.textAlign = 'center';
+  cardWrapper.appendChild(cardSupportingText);
+
+  /* var cardSaveButton = document.createElement('button');
+  cardSaveButton.textContent = 'Save';
+  cardSaveButton.className = 'mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent';
+  cardSupportingText.appendChild(cardSaveButton);
+  cardSaveButton.addEventListener('click', onSaveButtonClicked); */
+
+  componentHandler.upgradeElement(cardWrapper);
+  sharedMomentsArea.appendChild(cardWrapper);
+}
+
+fetch('https://httpbin.org/get')
+  .then(function (res) {
+    return res.json();
+  })
+  .then(function (data) {
+    createCard();
+  });
